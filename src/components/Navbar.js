80180644413aab9d22d0eb70f551433f5819001f
Brandon Garza <brandon.rey.garza@outlook.com>
2022-02-21 19:19:08 -0800

    organized files

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="Navbar">
+                {/* TODO */}
+                This is a navbar
+            </nav>
+        </div>
+    )
+}
